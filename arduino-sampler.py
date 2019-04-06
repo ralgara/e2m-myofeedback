8367a533b785d326835f1c5c5561ee841c188370
Rafael Algara <ralgara@gmail.com>
2019-04-06 00:21:13 -0400

    Basic Arduino sampler

--- arduino-sampler.py
+++ arduino-sampler.py
@@ -2,7 +2,14 @@
 
 #pip3 install pyserial
 import serial
+import sys
 
 with serial.Serial('/dev/cu.usbmodem14401', 19200, timeout=5) as s:
     for line in s:
-        print(line)
+        payload  = line.decode('utf-8').rstrip()
+        try:
+           adc = int(payload)
+           print(adc)
+           sys.stdout.flush()
+        except ValueError:
+           pass # print only parsed ints
