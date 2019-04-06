06c09ff7391b8a9d96736c9eafbc0ddc27ebf6bd
Rafael Algara <ralgara@gmail.com>
2019-04-06 00:05:09 -0400

    Extract Python for Arduino sampler

--- arduino-sampler.py
+++ arduino-sampler.py
@@ -3,6 +3,6 @@
 #pip3 install pyserial
 import serial
 
-with serial.Serial('/dev/cu.usbmodem142301', 19200, timeout=5) as s:
+with serial.Serial('/dev/cu.usbmodem14401', 19200, timeout=5) as s:
     for line in s:
         print(line)
